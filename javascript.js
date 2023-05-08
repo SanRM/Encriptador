6959fe64f009b65a7fdb5e9027b5d946ee89d473
SanRM <santiagoromom95@gmail.com>
2023-05-08 04:00:46 -0500

    responsive finished

--- javascript.js
+++ javascript.js
@@ -1,44 +1,57 @@
 
 
-function Encriptar() {
+function Encriptar_Desencriptar(opt) {
+
+
     var textareaValue = document.querySelector("#textarea_Encriptador").value;
-  
-    if (textareaValue != '') {
-        var div = document.querySelector("#divDesencriptador");
+    var regex = /^[a-z0-9\s]+$/;
 
-        // Reemplazamos todas las letras "a" por la letra "x"
-        var nuevoContenido = textareaValue.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
 
-        div.textContent = nuevoContenido;
-        div.style.paddingTop = "5%";
-        div.style.paddingBottom = "52%";
-        div.style.color = "#495057";
-        div.style.fontSize = "1.3em";
+    if (opt == 'encriptar'){
 
-        var boton = document.querySelector('.boton_3');
-        boton.style.display = 'block';
+        if (textareaValue !== '' && regex.test(textareaValue) && !(/[A-Z!@#$%^&*(),.?":{}|<>]/.test(textareaValue))) {
+            var div = document.querySelector("#divDesencriptador");
+
+            // Reemplazamos todas las letras "a" por la letra "x"
+            var nuevoContenido = textareaValue.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+
+            div.textContent = nuevoContenido;
+            div.style.color = "#495057";
+            div.style.fontSize = "1.3em";
+            div.style.maxWidth = "100%";
+            div.style.overflow = 'auto';
+
+            var boton = document.querySelector('.boton_3');
+            boton.style.display = 'block';
+        } else {
+            alert("El texto no puede contener mayúsculas o caracteres especiales");
+        }
 
     }
 
-}
 
-function Desencriptar() {
-    var textareaValue = document.querySelector("#textarea_Encriptador").value;
-  
-    if (textareaValue != '') {
-        var div = document.querySelector("#divDesencriptador");
-    
-        // Reemplazamos todas las letras "a" por la letra "x"
-        var nuevoContenido = textareaValue.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
-    
-        div.textContent = nuevoContenido;
-        div.style.paddingTop = "5%";
-        div.style.paddingBottom = "52%";
-        div.style.color = "#495057";
-        div.style.fontSize = "1.3em";
+    if (opt == 'desencriptar'){
 
+        if (textareaValue !== '' && regex.test(textareaValue) && !(/[A-Z!@#$%^&*(),.?":{}|<>]/.test(textareaValue))) {
+            var div = document.querySelector("#divDesencriptador");
+
+            // Reemplazamos todas las letras "a" por la letra "x"
+            var nuevoContenido = textareaValue.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+
+            div.textContent = nuevoContenido;
+            div.style.color = "#495057";
+            div.style.fontSize = "1.3em";
+            div.style.maxWidth = "100%";
+            div.style.overflow = 'auto';
+
+            var boton = document.querySelector('.boton_3');
+            boton.style.display = 'block';
+        } else {
+            alert("El texto no puede contener mayúsculas o caracteres especiales");
+        }
     }
 }
+
 
 function copiar(){
 
